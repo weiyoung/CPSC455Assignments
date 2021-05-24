d1a893f9aba55fdf79b97e25e03d90c11051ef32
Weiyoung Tan <wtan@bgcengineering.ca>
2021-05-24 12:39:35 -0700

    refactor, updated styles

--- script.js
+++ script.js
@@ -9,47 +9,47 @@ document.addEventListener('submit', (e)=>{
 
     if (name.length < 1 || url.length < 1 || desc.length < 1) {
         alert("Please ensure all fields are filled in!");
-    } else {
-        let newCard = {
-            name: name,
-            url: url,
-            desc: desc
-        }
-        cards.push(newCard);
+        return;
+    }
 
-        let thisCard = `<li class="card">
+    const newCard = {
+        name: name,
+        url: url,
+        desc: desc
+    }
+    cards.push(newCard);
+
+    const thisCard =    `<li class="card">
                         <img src="${newCard.url}" alt="${newCard.name}">
                         <div class="card-body">
                             <h4>${newCard.name}</h4>
                             <p>${newCard.desc}</p>
                         </div>
                         </li>`;
-        
-        const cardsContainer = document.getElementById('cards-container');
-        cardsContainer.innerHTML += thisCard;
+    
+    const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML += thisCard;
 
-        document.querySelector('form').reset();
-        localStorage.setItem('MyCards', JSON.stringify(cards));
-    }
+    document.querySelector('form').reset();
+    localStorage.setItem('MyCards', JSON.stringify(cards));
 });
 
 function showAll() {
     const cardsContainer = document.getElementById('cards-container');
     cardsContainer.innerHTML = "";
-    let shrekCard = `<li class="card">
-                    <img src="https://i.imgflip.com/18pk4c.jpg" alt="">
-                    <div class="card-body">
-                        <h4>Shrek</h4>
-                        <p>This is my <s>card</s> swamp! You won't be able to delete it.</p>
-                    </div>
-                    </li>`;
+    const shrekCard =   `<li class="card">
+                        <img src="https://i.imgflip.com/18pk4c.jpg" alt="">
+                        <div class="card-body">
+                            <h4>Shrek</h4>
+                            <p>This is my <s>card</s> swamp! You won't be able to delete it.</p>
+                        </div>
+                        </li>`;
     cardsContainer.innerHTML += shrekCard;
     
-    let myCards = localStorage.getItem('MyCards');
-    myCards = JSON.parse(myCards);
+    const myCards = JSON.parse(localStorage.getItem('MyCards'));
     if (myCards == null) return;
     for (const card of myCards) {
-        let thisCard = `<li class="card">
+        const thisCard = `<li class="card">
                         <img src="${card.url}" alt="${card.name}">
                         <div class="card-body">
                             <h4>${card.name}</h4>
