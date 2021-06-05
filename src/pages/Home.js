96cfb75c139dcdc1c6a0622fe1443d1a76392ea0
Weiyoung Tan <wtan@bgcengineering.ca>
2021-06-04 23:52:01 -0700

    updated styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
+import '../css/App.css';
 import Form from "../components/Form"
 
 const Home = () => {
     return (
         <div className={"container"}>
+            <h1>Add a card to the collection!</h1>
             <Form />
         </div>
     )
